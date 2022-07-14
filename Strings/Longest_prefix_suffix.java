0a567bf532e604797f2865270bea2a53066adeea
Debojyoti Mallick <debojyotimallick777@gmail.com>
2022-07-14 13:38:54 +0530

    Updated Code

--- Strings/Longest_prefix_suffix.java
+++ Strings/Longest_prefix_suffix.java
@@ -1,14 +1,36 @@
 package Strings;
 
+import java.util.Arrays;
+
 public class Longest_prefix_suffix {
     
-    static int longest(String str)
+    static String longest(String s)
     {
-        
-        return 0;
+        char arr[] = s.toCharArray();
+        int n = arr.length;
+        int[] lps = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            int j = lps[i-1];
+            while(j>0 && arr[j] != arr[i])
+                j = lps[j-1];
+            System.out.println(j);
+            if(arr[j] == arr[i])
+                j++;
+
+            lps[i] = j;
+            System.out.println(Arrays.toString(lps));
+        }
+        int j  =  lps[n-1];
+        StringBuffer sdf = new StringBuffer() ;
+        for (int i = 0; i < j; i++) {
+            sdf.append(arr[i]);
+        }
+        return sdf.toString(); 
     }
 
     public static void main(String[] args) {
-        
+        String s = "ababab";
+        System.out.println(longest(s));
     }
 }
