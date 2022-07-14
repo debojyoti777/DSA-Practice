abac5c3bfc30155d3fe3e32b86374016cbfa637a
Debojyoti Mallick <debojyotimallick777@gmail.com>
2022-07-14 12:11:57 +0530

    Commit message

---
+++ Strings/Longest_prefix_suffix.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class Longest_prefix_suffix {
+    
+    static int longest(String str)
+    {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
